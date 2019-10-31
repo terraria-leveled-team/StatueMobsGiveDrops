e6ae66ed65ceb2c57b9330d6804656eaaa68ded1
webmilio <therealwebmilio@gmail.com>
2019-10-31 12:10:20 -0400

    no message

---
+++ SMGDConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.IO;
+using Terraria;
+using Terraria.IO;
+using Terraria.ModLoader.Config;
+
+namespace StatueMobsGiveDrops
+{
+    public sealed class SMGDConfiguration : ModConfig
+    {
+        [Label("Gold Percentage"), Tooltip("The percentage of gold that the enemy should give when killed.")]
+        [ReloadRequired]
+        [Range(0, 1), DefaultValue(0)]
+        public float GoldPercentage { get; set; }
+
+        [Label("Give Drops")]
+        [ReloadRequired]
+        [DefaultValue(true)]
+        public bool GiveDrops { get; set; }
+
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+    }
+}
--- SMGDGlobalNPC.cs
+++ SMGDGlobalNPC.cs
@@ -1,15 +1,44 @@
-﻿using Terraria;
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace StatueMobsGiveDrops
 {
     public sealed class SMGDGlobalNPC : GlobalNPC
     {
+        private static readonly Dictionary<int, NPC> _baseNPCs = new Dictionary<int, NPC>();
+        private static List<NPC> _ranNPCs = new List<NPC>(Main.npc.Length);
+
         public override bool PreAI(NPC npc)
         {
-            npc.SpawnedFromStatue = false;
+            const bool returnValue = true;
 
-            return base.PreAI(npc);
+            if (!npc.SpawnedFromStatue) return returnValue;
+
+            if (!_baseNPCs.ContainsKey(npc.type))
+            {
+                NPC baseNPC = (NPC)Activator.CreateInstance(npc.GetType());
+                baseNPC.SetDefaults(npc.type);
+
+                _baseNPCs.Add(npc.type, baseNPC);
+            }
+
+            if (_ranNPCs.Contains(npc)) return returnValue;
+
+            npc.SpawnedFromStatue = !mod.GetConfig<SMGDConfiguration>().GiveDrops;
+            npc.value = _baseNPCs[npc.type].value * mod.GetConfig<SMGDConfiguration>().GoldPercentage;
+
+            _ranNPCs.Add(npc);
+
+            return returnValue;
+        }
+
+        public override bool PreNPCLoot(NPC npc)
+        {
+            _ranNPCs.Remove(npc);
+
+            return base.PreNPCLoot(npc);
         }
     }
 }
